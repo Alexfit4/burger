c08a97d6cd5e976f3824211ba4c567a671b97979
alexfit4 <alex.ashtiany@gmail.com>
2021-02-13 22:09:26 -0500

    handlebars

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 //* Required Modules/Packages
-const cTable = require("console.table");
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const confirm = require("inquirer-confirm");
+const express = require('express');
 
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them.
+const routes = require('./controllers/burger_controller.js');
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`));
